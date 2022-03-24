03f8e82e28e3e0e65dda560d54fa757318c000c1
Alina Dzhaafar <dzhaafar.alina@gmail.com>
2022-03-24 11:28:23 +0200

    Added country divs

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-//drop down menu filter by region
-//search country input bar
-//
+
+//selectors
+const gridContainer = document.querySelector('.grid-container')
+
+
+async function countryData() {
+    const response = await fetch('https://restcountries.com/v2/all');
+    const data = await response.json();
+    for (const country in data) {
+        const countryGrid = document.createElement('div');
+        gridContainer.appendChild(countryGrid);
+        countryGrid.innerHTML = 
+        `<div class="grid-item">
+            <img class="flag" src="${data[country].flags.png}" alt="flag">
+            <p class="country">${data[country].name}</p>
+            <div class="extra-info">
+                <p class="population"><b>Population:</b> ${data[country].population}</p>
+                <p class="region"><b>Region:</b> ${data[country].region}</p>
+                <p class="capital"><b>Capital:</b> ${data[country].capital}</p>
+        </div>`
+    }
+}
+
+countryData();
+
+
