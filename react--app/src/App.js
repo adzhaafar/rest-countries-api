e25cb956a75df77f3ddbf319c6f3c80a02a42fa4
Alina Dzhaafar <dzhaafar.alina@gmail.com>
2022-04-06 14:28:26 +0300

    Added new css files

--- react--app/src/App.js
+++ react--app/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-import Navbar from "./Components/Navbar.js" 
+import Navbar from "./Components/Navbar" 
+import SearchBar from "./Components/SearchBar"
+import FilterRegion from "./Components/FilterRegion"
+import CountryDivs from "./Components/CountryDivs";
 
 
 
 export default function App() {
   return (
-    < Navbar /> 
+    <div>
+      < Navbar /> 
+      < SearchBar />
+      < FilterRegion />
+      < CountryDivs /> 
+    </div>    
   )
 }
--- react--app/src/Components/Navbar.js
+++ react--app/src/Components/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react" 
 
+import "./Styles/Navbar.css"
+
 export default function Navbar() {
     return (
-        <nav>
+        <nav className="flex navbar">
             <h3>Where in the world?</h3>
             <div className="">
                 <ion-icon className=""></ion-icon>
-                <p>Dark Mode</p>
+                <p className="text-light">Dark Mode</p>
             </div>
         </nav>
     )
