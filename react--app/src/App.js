9b6d87b61ea0fd0b0bc3963733b18960c99de6e0
Alina Dzhaafar <dzhaafar.alina@gmail.com>
2022-04-07 20:11:58 +0300

    End of first page layout

--- react--app/src/App.js
+++ react--app/src/App.js
@@ -7,16 +7,36 @@ import CountryDivs from "./Components/CountryDivs";
 
 export default function App() {
 
-  // const [countryData, setCountryData] = React.useState({
+  const [countryData, setCountryData] = React.useState([]);
 
-  // })
-  // call the api here
+
+  React.useEffect(() => {
+    fetch("https://restcountries.com/v2/all")
+    .then(res => res.json())
+    .then(data => setCountryData(data))
+  }, [])
+
+  const countries = countryData.map(country => {
+    return (
+      <CountryDivs
+        key={country.name}
+        flag={country.flags.png}
+        name={country.name}
+        population={country.population}
+        region={country.region}
+        capital={country.capital} />
+    )
+  })
+
 
   return (
     <div>
       < Navbar /> 
       < SearchAndFilter />
-      < CountryDivs /> 
+      <section className="grid-container">
+        { countries}
+      </section>
     </div>    
   )
 }
+
--- react--app/src/Components/CountryDivs.js
+++ react--app/src/Components/CountryDivs.js
@@ -3,18 +3,18 @@ import React from "react";
 import "./Styles/CountryDivs.css"
 
 export default function CountryDivs(props) {
+
     return (
-    <div className="grid-container">  
         <div className="white-background border-radius box-shadow grid-item">
-            <img className="flag" src="{props.flags.png}" alt="flag"></img>
-            <p className="country-name line-space-big fs-large fw-dark letter-spacing">Country Name</p>
+            <img className="flag" src={props.flag} alt="flag"></img>
+                <p className="country-name line-space-big fs-large fw-dark letter-spacing">{props.name}</p>
             <div className="country-info">
-                <p className="line-space fs-medium fw-light"><span className="fw-dark">Population:</span> 1 million</p>
-                <p className="line-space fs-medium fw-light"><span className="fw-dark">Region:</span> Region</p>
-                <p className="line-space fs-medium fw-light"><span className="fw-dark">Capital:</span> Capital</p>
+                    <p className="line-space fs-medium fw-light"><span className="fw-dark">Population: </span>{props.population}</p>
+                    <p className="line-space fs-medium fw-light"><span className="fw-dark">Region: </span>{props.region}</p>
+                    <p className="line-space fs-medium fw-light"><span className="fw-dark">Capital: </span>{props.capital}</p>
             </div>              
-        </div>  
-    </div>              
+        </div> 
+                
 )
 }
 
